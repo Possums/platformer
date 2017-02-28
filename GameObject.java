c3183d09ccdb40a5bad7c8d4cd28b0c7753aeece
Patrick Lin <tapniltap@gmail.com>
2017-02-28 11:54:39 -0800

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -11,8 +11,7 @@ public abstract class GameObject implements MovingObject {
 	
 	
 		size, // 10 might be a good size   
-		health, // 0 - 100
-		power;// not sure about this...
+		health; // 0 - 100
 	private int level;//
 	private Color color;
 	private Image img;
@@ -20,12 +19,25 @@ public abstract class GameObject implements MovingObject {
 	@Override
 	public void move() {
 		x+= speed*Math.cos(direction);
-		
+		y+= speed*Math.sin(direction);
+		resetDir();
+		gravity();
 		checkOffScreen();
 		// maybe "push" back onto the screen change direction if
 		// this object goes off the screen
 	}
-
+	public void gravity() {
+		if(direction>(3*Math.PI/2)||direction<(Math.PI/2))
+			direction --;
+		if(direction<(3*Math.PI/2)&&direction>(Math.PI/2))
+			direction ++;
+	}
+	public void resetDir() {
+		if(direction>2*Math.PI)
+			direction -= 2*Math.PI;
+		if(direction<2*Math.PI)
+			direction += 2*Math.PI;
+	}
 	public abstract void checkOffScreen();
 	
 	
