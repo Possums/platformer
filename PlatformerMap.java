ac9e9e0b12070c8d35d21586343c5d2f40800922
Possums <brushtailpossums@gmail.com>
2017-03-01 12:28:09 -0800

    Add files via upload

--- PlatformerMap.java
+++ PlatformerMap.java
@@ -1,18 +1,37 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.net.URL;
+
+
+import javax.imageio.ImageIO;
 
 public class PlatformerMap extends GameMap {
 
-	
+	Image background;
+	Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
 	public PlatformerMap() {
-		
-		
-		
+
+
+
 	}
 
 
 	@Override
 	public void openBackgroundImage() {
 		// TODO Auto-generated method stub
+		try {
+			URL url = getClass().getResource("images/background.jpg");
+			background = ImageIO.read(url);
+		} catch (Exception e) {
+			System.out.println("problem opening the background");
+			e.printStackTrace();
+		}	
+	}
 
+	public void draw(Graphics g){
+		g.drawImage(background, 0,0, 3840, 2160, null);
 	}
 
 }
