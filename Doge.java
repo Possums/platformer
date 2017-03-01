cc79aa6ac718309730b26a5293b0e4b8d709d1f6
Patrick Lin <tapniltap@gmail.com>
2017-03-01 12:31:45 -0800

    Add files via upload

--- Doge.java
+++ Doge.java
@@ -1,18 +1,30 @@
+import java.awt.Color;
 import java.awt.Graphics;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
 
 
 public class Doge extends GameObject {
-
+	public Doge(){
+		try {                
+			URL url = getClass().getResource("images/doge.png");
+			setImg(ImageIO.read(url));
+	       } catch (IOException ex) {
+	            // handle exception...
+	       }
+	}
 	@Override
 	public void checkOffScreen() {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		getImg().getHeight(null);
+		g.drawImage(getImg(), 0, 0, null);
 	}
 	
 }
