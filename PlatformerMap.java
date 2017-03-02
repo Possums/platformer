679a20386ac7f2f070c53ef48e397f3e8da0911f
Possums <brushtailpossums@gmail.com>
2017-03-02 11:58:58 -0800

    Add files via upload

--- PlatformerMap.java
+++ PlatformerMap.java
@@ -9,12 +9,12 @@ import javax.imageio.ImageIO;
 
 public class PlatformerMap extends GameMap {
 
-	Image background;
+	Image background, platform;
 	Dimension a = Toolkit.getDefaultToolkit().getScreenSize();
 	int mapX = a.width;
 	int mapY = a.height;
 	public PlatformerMap() {
-		addGameObject(new Doge(Math.random(),Math.random()*Math.PI*2, 100, 3, x, x));
+		addGameObject(new Doge(Math.random(),Math.random()*Math.PI*2, 100, 3, 0, 0));
 	}
 
 
@@ -27,11 +27,22 @@ public class PlatformerMap extends GameMap {
 		} catch (Exception e) {
 			System.out.println("problem opening the background");
 			e.printStackTrace();
+		}
+		
+		try {
+			URL url = getClass().getResource("images/platform.png");
+			platform = ImageIO.read(url);
+		} catch (Exception e) {
+			System.out.println("problem opening the background");
+			e.printStackTrace();
 		}	
+		
 	}
 
 	public void draw(Graphics g){
 		g.drawImage(background, 0,0, mapX, mapY, null);
+		g.drawImage(platform, 0 , 3*mapY/4, mapX/2, mapY/8, null);
+		g.drawImage(platform, mapX/2 , 3*mapY/4, mapX/2, mapY/8, null);
 		super.draw(g);
 	}
 
